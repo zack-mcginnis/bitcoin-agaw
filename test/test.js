53dc3f37c16c91d85ba4226f64fc43e307f5a048
zack <zackmcginnis@hotmail.com>
2019-07-07 16:50:50 -0700

    updates to test

--- test/test.js
+++ test/test.js
@@ -1,15 +1,17 @@
 const Bitcoin = require(`../src/bitcoin.service`)
+const expect = require("chai").expect;
 
-const btc = new Bitcoin(`xpub661MyMwAqRbcFtXgS5sYJABqqG9YLmC4Q1Rdap9gSE8NqtwybGhePY2gZ29ESFjqJoCu1Rupje8YtGqsefD265TMg7usUDFdp6W1EGMcet8`)
+describe("Address generation", () => {
+    describe("With valid xpub", () => {
+        it("should generate 2 addresses", () => {
 
-let count = 1
+            const btc = new Bitcoin(`xpub661MyMwAqRbcFtXgS5sYJABqqG9YLmC4Q1Rdap9gSE8NqtwybGhePY2gZ29ESFjqJoCu1Rupje8YtGqsefD265TMg7usUDFdp6W1EGMcet8`)
+            let count = 1
 
+            const address1 = btc.generate(count++)
+            const address2 = btc.generate(count++)
 
-btc.generate(count++)
-btc.generate(count++)
-btc.generate(count++)
-btc.generate(count++)
-btc.generate(count++)
-btc.generate(count++)
-btc.generate(count++)
-btc.generate(count++)
+            expect(address1).to.not.equal(address2);
+        });
+    });
+});
