39352c98f0e07b65c8e14f4bd22763a30ac6dfa2
zack <zackmcginnis@hotmail.com>
2019-07-07 16:40:32 -0700

    refactor src and test

--- test/test.js
+++ test/test.js
@@ -1,4 +1,4 @@
-const Bitcoin = require(`../src/bitcoin`)
+const Bitcoin = require(`../src/bitcoin.service`)
 
 const btc = new Bitcoin(`xpub661MyMwAqRbcFtXgS5sYJABqqG9YLmC4Q1Rdap9gSE8NqtwybGhePY2gZ29ESFjqJoCu1Rupje8YtGqsefD265TMg7usUDFdp6W1EGMcet8`)
 
